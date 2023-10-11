b25f89bd41c00fa26c8f4b7fca52120353561fff
Damiano Pavon <damianopavon@gmail.com>
2023-10-11 21:10:06 +0200

    d1_w1_m2

---
+++ domandecompito.js
@@ -0,0 +1,5 @@
+// Un algoritmo è un procedimento che permette di eseguire calcoli in modo sistematico e automatico
+
+// Una variabile è un elemento il cui valore può essere modificato nel tempo
+
+// No perchè Undefined indica che non ci sono le informazioni necessarie, mentre Null indica la NON assegnazione del suo valore
